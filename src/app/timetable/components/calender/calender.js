85947b4cf1adfae3327b124abc951999eefa53be
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-08-15 13:52:50 +1200

    feat: Implemented creating/deleting events, and event drag-drop functionality.

--- src/app/timetable/components/calender/calender.js
+++ src/app/timetable/components/calender/calender.js
@@ -2,15 +2,57 @@
 
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { INITIAL_EVENTS, createEventId } from "./event-utils";
 
 export default function Calendar() {
   return (
     <FullCalendar
-      plugins={[timeGridPlugin]}
+      plugins={[timeGridPlugin, interactionPlugin]}
       initialView="timeGridWeek"
       editable={true}
       selectable={true}
+      initialEvents={INITIAL_EVENTS}
       height={650}
+      select={handleDateSelect}
+      eventContent={renderEventContent}
+      eventClick={handleEventClick}
     />
   );
+
+  function handleDateSelect(selectInfo) {
+    let title = prompt("Please enter a new title for your event");
+    let calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  }
+
+  function handleEventClick(clickInfo) {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  }
+
+  function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  }
 }
