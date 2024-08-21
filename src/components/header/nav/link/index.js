48ef8dee3c297deb1949ab062223a6333e82276c
hyukjun3 <ckwo873@aucklanduni.ac.nz>
2024-08-22 01:58:05 +1200

    style: Nav bar now has responsive links with arrows.

--- src/components/header/nav/link/index.js
+++ src/components/header/nav/link/index.js
@@ -1,19 +1,30 @@
-import Link from "next/link";
-import { menuSlide, slide } from "../../anim";
+import React from "react";
 import { motion } from "framer-motion";
 import "./headerLink.css";
 
-import PropTypes from "prop-types";
-
-export default function Index({ data }) {
-  Index.propTypes = {
-    data: PropTypes.object.isRequired,
-  };
+export default function Link({ data }) {
   return (
-    <motion.div variants={slide} animate="enter" exit="exit" initial="initial">
-      <Link href={data.href} className="small-font">
-        {data.title}
-      </Link>
-    </motion.div>
+    <motion.a
+      href={data.href}
+      className="nav-link"
+      whileHover={{ x: 15 }}
+      whileTap={{ scale: 0.95 }}
+    >
+      {data.title}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="currentColor"
+        className="arrow-icon"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M8.25 4.5l7.5 7.5-7.5 7.5"
+        />
+      </svg>
+    </motion.a>
   );
 }
