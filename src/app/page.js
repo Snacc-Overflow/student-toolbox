1b491a504934be6efbd77d77062e206efb55f5e0
tmacsmee <troymacsmee@gmail.com>
2024-08-21 19:19:31 +1200

    feat: register and login using middleware

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,11 @@
-import { auth } from "@/auth"
-import DatabaseExample from "@/components/dashboard/database-example"
-import Title from "@/components/title"
+import DatabaseExample from "@/components/dashboard/database-example";
+import Title from "@/components/title";
 
 export default async function Home() {
-  const session = await auth()
-
   return (
     <main>
-      <Title
-        text={`👋 Welcome, ${session ? session?.user?.name : "student"}!`}
-      />
+      <Title text={`👋 Welcome, "student"`} />
       <DatabaseExample />
     </main>
-  )
+  );
 }
--- src/components/auth/layout-avatar.jsx
+++ src/components/auth/layout-avatar.jsx
@@ -1,15 +1,17 @@
-import { auth } from "@/auth"
-import styles from "./auth.module.scss"
-import Avatar from "./avatar"
+"use client";
 
-export default async function LayoutAvatar() {
-  const session = await auth()
+import { useSession } from "next-auth/react";
+import styles from "./auth.module.scss";
+import Avatar from "./avatar";
 
-  if (!session?.user?.name) return null
+export default function LayoutAvatar() {
+  const session = useSession();
+
+  if (!session?.data?.user?.name) return null;
 
   return (
     <div className={styles["layout-avatar"]}>
-      <Avatar size={48} username={session} />
+      <Avatar size={48} username={session.data.user.name} />
     </div>
-  )
+  );
 }
